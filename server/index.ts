08eb6eeebe0fa26fd0c4a1f21ed382598d4333e9
Maher-Guerfali <maher.guerfali@gmail.com>
2025-04-23 13:36:11 +0100

    first commigkkddt

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://elkarritanew.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
+  
 app.use(express.json());
 
 // Connect to MongoDB using the URI from your .env file
