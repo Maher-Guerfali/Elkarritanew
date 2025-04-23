c734b9b997bf354c97cd2ed5eaeee1f2f68fc54a
Maher-Guerfali <maher.guerfali@gmail.com>
2025-04-23 13:37:08 +0100

    first commigkkddet

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://elkarritanew.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://elkarritanew.vercel.app/','elkarritanew-narstvi9k-maherguerfalis-projects.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }));
